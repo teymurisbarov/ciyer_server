566138b18d88c902b94e0cd70b3af28accd45f93
teymurisbarov <142144884+teymurisbarov@users.noreply.github.com>
2026-02-18 18:44:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const http = require('http');
 // 1. RENDER ÜÇÜN PORT VƏ SERVER AYARLARI
 const PORT = process.env.PORT || 3000; 
-
 const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end("Seka Server is Live!");
