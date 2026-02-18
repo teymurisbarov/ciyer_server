7aa2249a8e2c6ce7f401d3fad7c3b0961fa82318
teymurisbarov <142144884+teymurisbarov@users.noreply.github.com>
2026-02-18 17:58:46 +0400

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose');
 
 // --- MONGODB BAĞLANTISI ---
 // 'seka_game' adlı verilənlər bazasına bağlanır
-mongoose.connect('mongodb+srv://teymurisbarov:teymurisbarov@cluster0.1xrr77f.mongodb.net/?appName=Cluster0')
-    .then(() => console.log("MongoDB-yə uğurla bağlanıldı"))
-    .catch(err => console.error("MongoDB xətası:", err));
+mongoose.connect('mongodb+srv://teymurisbarov:teymurisbarov@cluster0.1xrr77f.mongodb.net/seka_game?retryWrites=true&w=majority')
+    .then(() => console.log("✅ MongoDB-yə uğurla bağlanıldı"))
+    .catch(err => console.error("❌ MongoDB bağlantı xətası:", err));
 
 // İstifadəçi Modeli
 const UserSchema = new mongoose.Schema({
